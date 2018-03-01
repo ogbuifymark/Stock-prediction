939a6b2f244127df1baeaf91a404943bc3639b55
Ogbuify Markanthony <ogbuifymark@gmail.com>
2018-03-01 16:55:59 +0100

    made changes

--- BitcoinPricePrediction.py
+++ BitcoinPricePrediction.py
@@ -38,9 +38,145 @@ bitcoin_im = Image.open(image_file)
 
 image_file = io.BytesIO(eth_img.read())
 eth_im = Image.open(image_file)
-width_eth_im , height_eth_im  = eth_im.size
+width_eth_im, height_eth_im  = eth_im.size
 eth_im = eth_im.resize((int(eth_im.size[0]*0.6), int(eth_im.size[1]*0.6)), Image.ANTIALIAS)
 
 bitcoinMarketInfo.columns =[bitcoinMarketInfo.columns[0]]+['bt_'+i for i in bitcoinMarketInfo.columns[1:]]
-print(bitcoinMarketInfo.columns)
+
 ethMarkeInfo.columns =[ethMarkeInfo.columns[0]]+['eth_'+i for i in ethMarkeInfo.columns[1:]]
+
+fig, (ax1, ax2) = plt.subplots(2,1, gridspec_kw = {'height_ratios':[3, 1]})
+ax1.set_ylabel('Closing Price ($)',fontsize=12)
+ax2.set_ylabel('Volume ($ bn)',fontsize=12)
+ax2.set_yticks([int('%d000000000'%i) for i in range(10)])
+ax2.set_yticklabels(range(10))
+ax1.set_xticks([datetime.date(i,j,1) for i in range(2013,2019) for j in [1,7]])
+ax1.set_xticklabels('')
+ax2.set_xticks([datetime.date(i,j,1) for i in range(2013,2019) for j in [1,7]])
+ax2.set_xticklabels([datetime.date(i,j,1).strftime('%b %Y')  for i in range(2013,2019) for j in [1,7]])
+ax1.plot(bitcoinMarketInfo['Date'].astype(datetime.datetime),bitcoinMarketInfo['bt_Open'])
+ax2.bar(bitcoinMarketInfo['Date'].astype(datetime.datetime).values, bitcoinMarketInfo['bt_Volume'].values)
+fig.tight_layout()
+fig.figimage(bitcoin_im, 100, 120, zorder=3,alpha=.5)
+# plt.show()
+
+fig, (ax1, ax2) = plt.subplots(2,1, gridspec_kw = {'height_ratios':[3, 1]})
+#ax1.set_yscale('log')
+ax1.set_ylabel('Closing Price ($)',fontsize=12)
+ax2.set_ylabel('Volume ($ bn)',fontsize=12)
+ax2.set_yticks([int('%d000000000'%i) for i in range(10)])
+ax2.set_yticklabels(range(10))
+ax1.set_xticks([datetime.date(i,j,1) for i in range(2013,2019) for j in [1,7]])
+ax1.set_xticklabels('')
+ax2.set_xticks([datetime.date(i,j,1) for i in range(2013,2019) for j in [1,7]])
+ax2.set_xticklabels([datetime.date(i,j,1).strftime('%b %Y')  for i in range(2013,2019) for j in [1,7]])
+ax1.plot(ethMarkeInfo['Date'].astype(datetime.datetime),ethMarkeInfo['eth_Open'])
+ax2.bar(ethMarkeInfo['Date'].astype(datetime.datetime).values, ethMarkeInfo['eth_Volume'].values)
+fig.tight_layout()
+fig.figimage(eth_im, 300, 180, zorder=3, alpha=.6)
+# plt.show()
+
+n = bitcoinMarketInfo.shape[0]
+p = ethMarkeInfo.shape[0]
+test_start = 0
+test_end = int(np.floor(0.1*n))
+train_start = test_end + 1
+train_end = n
+
+market_info = pd.merge(bitcoinMarketInfo,ethMarkeInfo, on=['Date'])
+# market_info = market_info[market_info['Date']>='2016-01-01']
+
+for coins in ['bt_', 'eth_']:
+    kwargs = { coins+'day_diff': lambda x: (x[coins+'Close']-x[coins+'Open'])/x[coins+'Open']}
+    market_info = market_info.assign(**kwargs)
+
+
+
+split_date = str((bitcoinMarketInfo.loc[test_end]['Date']).date())
+print(split_date)
+fig, (ax1, ax2) = plt.subplots(2,1)
+ax1.set_xticks([datetime.date(i,j,1) for i in range(2013,2019) for j in [1,7]])
+ax1.set_xticklabels('')
+ax2.set_xticks([datetime.date(i,j,1) for i in range(2013,2019) for j in [1,7]])
+ax2.set_xticklabels([datetime.date(i,j,1).strftime('%b %Y')  for i in range(2013,2019) for j in [1,7]])
+ax1.plot(market_info[market_info['Date'] < split_date]['Date'].astype(datetime.datetime),
+         market_info[market_info['Date'] < split_date]['bt_Close'],
+         color='#B08FC7', label='Training')
+ax1.plot(market_info[market_info['Date'] >= split_date]['Date'].astype(datetime.datetime),
+         market_info[market_info['Date'] >= split_date]['bt_Close'],
+         color='#8FBAC8', label='Test')
+ax2.plot(market_info[market_info['Date'] < split_date]['Date'].astype(datetime.datetime),
+         market_info[market_info['Date'] < split_date]['eth_Close'],
+         color='#B08FC7')
+ax2.plot(market_info[market_info['Date'] >= split_date]['Date'].astype(datetime.datetime),
+         market_info[market_info['Date'] >= split_date]['eth_Close'], color='#8FBAC8')
+ax1.set_xticklabels('')
+ax1.set_ylabel('Bitcoin Price ($)',fontsize=12)
+ax2.set_ylabel('Ethereum Price ($)',fontsize=12)
+plt.tight_layout()
+ax1.legend(bbox_to_anchor=(0.03, 1), loc=2, borderaxespad=0., prop={'size': 14})
+fig.figimage(bitcoin_im.resize((int(bitcoin_im.size[0]*0.65), int(bitcoin_im.size[1]*0.65)), Image.ANTIALIAS),
+             200, 260, zorder=3,alpha=.5)
+fig.figimage(eth_im.resize((int(eth_im.size[0]*0.65), int(eth_im.size[1]*0.65)), Image.ANTIALIAS),
+             350, 40, zorder=3,alpha=.5)
+# plt.show()
+
+
+fig, (ax1, ax2) = plt.subplots(2,1)
+ax1.set_xticks([datetime.date(2017,i+1,1) for i in range(12)])
+ax1.set_xticklabels('')
+ax2.set_xticks([datetime.date(2017,i+1,1) for i in range(12)])
+ax2.set_xticklabels([datetime.date(2017,i+1,1).strftime('%b %d %Y')  for i in range(12)])
+ax1.plot(market_info[market_info['Date']>= split_date]['Date'].astype(datetime.datetime),
+         market_info[market_info['Date']>= split_date]['bt_Close'].values, label='Actual')
+ax1.plot(market_info[market_info['Date']>= split_date]['Date'].astype(datetime.datetime),
+          market_info[market_info['Date']>= datetime.datetime.strptime(split_date, '%Y-%m-%d') -
+                      datetime.timedelta(days=1)]['bt_Close'][1:].values, label='Predicted')
+ax1.set_ylabel('Bitcoin Price ($)',fontsize=12)
+ax1.legend(bbox_to_anchor=(0.1, 1), loc=2, borderaxespad=0., prop={'size': 14})
+ax1.set_title('Simple Lag Model (Test Set)')
+ax2.set_ylabel('Etherum Price ($)',fontsize=12)
+ax2.plot(market_info[market_info['Date']>= split_date]['Date'].astype(datetime.datetime),
+         market_info[market_info['Date']>= split_date]['eth_Close'].values, label='Actual')
+ax2.plot(market_info[market_info['Date']>= split_date]['Date'].astype(datetime.datetime),
+          market_info[market_info['Date']>= datetime.datetime.strptime(split_date, '%Y-%m-%d') -
+                      datetime.timedelta(days=1)]['eth_Close'][1:].values, label='Predicted')
+fig.tight_layout()
+plt.show()
+
+
+fig, (ax1, ax2) = plt.subplots(1,2)
+ax1.hist(market_info[market_info['Date']< split_date]['bt_day_diff'].values, bins=100)
+ax2.hist(market_info[market_info['Date']< split_date]['eth_day_diff'].values, bins=100)
+ax1.set_title('Bitcoin Daily Price Changes')
+ax2.set_title('Ethereum Daily Price Changes')
+# plt.show()
+
+np.random.seed(202)
+bt_walk_mean, bt_walk_sd = np.mean(market_info[market_info['Date']< split_date]['bt_day_diff'].values),np.std(market_info[market_info['Date']< split_date]['bt_day_diff'].values)
+
+bt_random_steps = np.random.normal(bt_walk_mean, bt_walk_sd,(max(market_info['Date']).to_pydatetime() - datetime.datetime.strptime(split_date, '%Y-%m-%d')).days + 1)
+print(bt_random_steps)
+eth_walk_mean, eth_r_walk_sd = np.mean(market_info[market_info['Date']< split_date]['eth_day_diff'].values), np.std(market_info[market_info['Date'] < split_date]['eth_day_diff'].values)
+eth_random_steps = np.random.normal(eth_walk_mean, eth_r_walk_sd, (max(market_info['Date']).to_pydatetime() - datetime.datetime.strptime(split_date, '%Y-%m-%d')).days + 1)
+fig, (ax1, ax2) = plt.subplots(2,1)
+ax1.set_xticks([datetime.date(2017,i+1,1) for i in range(12)])
+ax1.set_xticklabels('')
+ax2.set_xticks([datetime.date(2017,i+1,1) for i in range(12)])
+ax2.set_xticklabels([datetime.date(2017,i+1,1).strftime('%b %d %Y') for i in range(12)])
+ax1.plot(market_info[market_info['Date']>= split_date]['Date'].astype(datetime.datetime),
+     market_info[market_info['Date']>= split_date]['bt_Close'].values, label='Actual')
+ax1.plot(market_info[market_info['Date']>= split_date]['Date'].astype(datetime.datetime),
+      market_info[(market_info['Date']+ datetime.timedelta(days=1))>= split_date]['bt_Close'].values[1:] *
+     (1+bt_random_steps), label='Predicted')
+ax2.plot(market_info[market_info['Date']>= split_date]['Date'].astype(datetime.datetime),
+     market_info[market_info['Date']>= split_date]['eth_Close'].values, label='Actual')
+ax2.plot(market_info[market_info['Date']>= split_date]['Date'].astype(datetime.datetime),
+      market_info[(market_info['Date']+ datetime.timedelta(days=1))>= split_date]['eth_Close'].values[1:] *
+     (1+eth_random_steps), label='Predicted')
+ax1.set_title('Single Point Random Walk (Test Set)')
+ax1.set_ylabel('Bitcoin Price ($)',fontsize=12)
+ax2.set_ylabel('Ethereum Price ($)',fontsize=12)
+ax1.legend(bbox_to_anchor=(0.1, 1), loc=2, borderaxespad=0., prop={'size': 14})
+plt.tight_layout()
+plt.show()
